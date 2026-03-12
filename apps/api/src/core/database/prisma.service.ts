1a1fae90713e2402e3af1587f42130c1857cf473
Brendon Lima <brwndonlima@gmail.com>
2026-03-12 11:04:52 -0300

    fix-prisma-runtime-import

--- apps/api/src/core/database/prisma.service.ts
+++ apps/api/src/core/database/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@solid-service/database';
+import { PrismaClient } from '@prisma/client';
 import { TenantContextService } from '../tenant';
 
 /**
